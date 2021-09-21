3a9f98041b891e6836525159ac2ceeb4a852b909
Chaarly99 <91130440+Chaarly99@users.noreply.github.com>
2021-09-21 14:43:14 +0200

    Add files via upload

--- codigo.js
+++ codigo.js
@@ -2,3 +2,10 @@ function saludar(){
     window.alert("has pulsado un boton");
     console.log("Boton ok");
 }
+
+function numeros(){
+    for (var i = 0; i <= 5; i++) {
+        var n =+ i;
+        console.log(i);
+    }
+}
